8a099a4b726e801a1a604b5a2dc2e54ce83552fa
MATA's Mac <matembebenjamin@gmail.com>
2023-05-31 17:49:49 +0300

    Edit calculator UI in Calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -16,31 +16,31 @@ export default function Calculator() {
         </div>
         <div className="between" />
         <div className="second">
-          <div className="light_grey center"><p>AC</p></div>
-          <div className="light_grey center"><p>+/-</p></div>
-          <div className="light_grey center"><p>%</p></div>
-          <div className="orange center"><p>/</p></div>
+          <div className="light_grey center"><p>7</p></div>
+          <div className="light_grey center"><p>8</p></div>
+          <div className="light_grey center"><p>9</p></div>
+          <div className="orange center"><p>X</p></div>
         </div>
         <div className="between" />
         <div className="second">
-          <div className="light_grey center"><p>AC</p></div>
-          <div className="light_grey center"><p>+/-</p></div>
-          <div className="light_grey center"><p>%</p></div>
-          <div className="orange center"><p>/</p></div>
+          <div className="light_grey center"><p>4</p></div>
+          <div className="light_grey center"><p>5</p></div>
+          <div className="light_grey center"><p>6</p></div>
+          <div className="orange center"><p>_</p></div>
         </div>
         <div className="between" />
         <div className="second">
-          <div className="light_grey center"><p>AC</p></div>
-          <div className="light_grey center"><p>+/-</p></div>
-          <div className="light_grey center"><p>%</p></div>
-          <div className="orange center"><p>/</p></div>
+          <div className="light_grey center"><p>1</p></div>
+          <div className="light_grey center"><p>2</p></div>
+          <div className="light_grey center"><p>3</p></div>
+          <div className="orange center"><p>+</p></div>
         </div>
         <div className="between" />
         <div className="second">
-          <div className="light_grey center"><p>AC</p></div>
-          <div className="light_grey center"><p>+/-</p></div>
-          <div className="light_grey center"><p>%</p></div>
-          <div className="orange center"><p>/</p></div>
+          <div className="light_grey center bigger"><p>0</p></div>
+          <div className="light_grey center"><p>.</p></div>
+          {/* <div className="light_grey center"><p></p></div> */}
+          <div className="orange center"><p>=</p></div>
         </div>
 
       </div>
