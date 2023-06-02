2f689237bbb0da761c22d757489ad816afabd73e
MATA's Mac <matembebenjamin@gmail.com>
2023-06-02 13:40:49 +0300

    Fetch quotes using useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import QuoteComponent from './components/Quote';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <QuoteComponent />
     </div>
   );
 }
